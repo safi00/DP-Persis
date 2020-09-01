c7f936bc744e768bbc606d4865361007fa901b2d
safi00 <safi_bb@hotmail.com>
2020-09-01 18:47:45 +0200

    Initial commit

---
+++ src/P2/Domain/Adres.java
@@ -0,0 +1,4 @@
+package P2.Domain;
+
+public class Adres {
+}
---
+++ src/P2/Domain/Reiziger.java
@@ -0,0 +1,69 @@
+package P2.Domain;
+
+import java.sql.Date;
+
+public class Reiziger {
+
+    private int    idnummer;
+    private String voorletters;
+    private String tussenvoegsel;
+    private String achternaam;
+    private Date   geboortedatum;
+
+    public Reiziger(int idnummer, String voorletters, String tussenvoegsel, String achternaam, String geboortedatum) {
+        this.idnummer      = idnummer;
+        this.voorletters   = voorletters;
+        this.tussenvoegsel = tussenvoegsel;
+        this.achternaam    = achternaam;
+        this.geboortedatum = Date.valueOf(geboortedatum);
+    }
+
+    public int getIdnummer(){
+        return idnummer;
+    }
+    public String getVoorletters(){
+        return voorletters;
+    }
+    public String getTussenvoegsel(){
+        return tussenvoegsel;
+    }
+    public String getAchternaam(){
+        return achternaam;
+    }
+    public Date getGeboortedatum(){
+        return geboortedatum;
+    }
+
+    public void setIdnummer(int id){
+        idnummer = id;
+    }
+    public void setVoorletters(String string){
+        voorletters = string;
+    }
+    public void setTussenvoegsel(String string){
+        tussenvoegsel = string;
+    }
+    public void setAchternaam(String string){
+        achternaam = string;
+    }
+    public void setGeboortedatum(Date datum){
+        geboortedatum = datum;
+    }
+
+    public String getNaam(){
+        String naam = voorletters + " ";
+        if (tussenvoegsel != null){
+            naam = naam + tussenvoegsel + " ";
+        }
+        naam = naam + achternaam;
+        return naam;
+    }
+
+    public String toString(){
+        return  "De Reiziger id: "              + idnummer +      ".\n" +
+                "De Reiziger's voorletters: "   + voorletters +   ".\n" +
+                "De Reiziger's tussenvoegsel: " + tussenvoegsel + ".\n" +
+                "De Reiziger's lastname: "      + achternaam +    ".\n" +
+                "De Reiziger was born on: "     + geboortedatum;
+    }
+}
